58bffc08eef84a0b2e39ea5b329991a698b7d78d
hoadepzay <dangquanghoa206@gmail.com>
2026-07-29 00:07:57 +0700

    Initial commit for Visual Finance Frontend

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CameraSnap from './components/CameraSnap'
+
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-black">
+      <CameraSnap />
+    </div>
+  )
+}
+
+export default App
